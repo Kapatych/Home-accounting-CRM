84d23d882733788bd73c24c7264116883eaf1561
Kapatych <onetruestory@ya.ru>
2020-06-27 18:33:59 +0300

    Added chart in history page (DoughnutChart)

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,10 +4,6 @@
       <h3>История записей</h3>
     </div>
 
-    <div class="history-chart">
-      <canvas></canvas>
-    </div>
-
     <Loader v-if="loading"/>
 
     <p class="center" v-else-if="!records.length">
@@ -16,6 +12,10 @@
     </p>
 
     <section v-else>
+      <div class="history-chart">
+        <DoughnutChart :chartdata="chartdata" />
+      </div>
+
       <HistoryTable :records="items" :startPageNumber="startPageNumber" />
 
       <Paginate v-if="pageCount > 1"
@@ -32,13 +32,15 @@
 <script>
   import PaginationMixin from "@/mixins/pagination.mixin";
   import HistoryTable from "@/components/HistoryTable";
+  import DoughnutChart from "@/components/DoughnutChart";
 
   export default {
     name: 'History',
     mixins: [PaginationMixin],
     data: () => ({
       loading: true,
-      records: []
+      records: [],
+      categories: []
     }),
     watch: {
       '$route'(to){
@@ -51,15 +53,37 @@
     computed: {
       startPageNumber() {
         return this.records.length - ((this.page - 1) * this.pageSize)
+      },
+      chartdata() {
+        return {
+          labels: this.categories.map(cat => cat.title),
+          datasets: [
+            {
+              label: 'Расходы по категориям',
+              data: this.categories.map(cat => {
+                return this.records.reduce((total, rec) => {
+                  if (rec.categoryId === cat.id && rec.type === 'outcome') {
+                    total += +rec.amount
+                  }
+                  return total
+                }, 0)
+              }),
+              backgroundColor: [
+                '#9c27b0', '#ffeb3b', '#2196f3', '#e91e63',
+                '#00bcd4', '#f44336', '#8bc34a', '#3f51b5',
+              ],
+            }
+          ]
+        }
       }
     },
     async mounted() {
-      const categories = await this.$store.dispatch('fetchCategories');
+      this.categories = await this.$store.dispatch('fetchCategories');
       this.records = await this.$store.dispatch('fetchRecords');
 
       this.initPagination(this.records.map(record => ({
         ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
+        categoryName: this.categories.find(c => c.id === record.categoryId).title,
         typeClass: record.type === 'income' ? 'green' : 'red',
         typeText: record.type === 'income' ? 'Доход' : 'Расход',
       })).reverse());
@@ -67,6 +91,7 @@
       this.loading = false;
     },
     components: {
+      DoughnutChart,
       HistoryTable
     }
   }
